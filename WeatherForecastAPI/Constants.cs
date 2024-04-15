049bd5748a48f3710c867a3b030de93fb6de5822
Musilová, Anežka <anezka.musilova@tul.cz>
2024-04-15 17:47:22 +0200

    Improve CityRepository

---
+++ WeatherForecastAPI/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherForecastAPI
+{
+    public class Constants
+    {
+        public static string citiesDataPath = Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data"), "cities.json");
+    }
+}
